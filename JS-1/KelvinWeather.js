6fbc021962445a92080b1dc0acdcd46a30412351
KasonC <kasonckm@gmail.com>
2021-04-21 16:49:14 +0800

    update commit

---
+++ JS-1/KelvinWeather.js
@@ -0,0 +1,16 @@
+//my age
+const myAge = 25;
+//earlyYears
+let earlyYears = 2;
+earlyYears *=10.5
+let laterYears = myAge - 2;
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears =earlyYears + laterYears;
+
+let myName = "Kason".toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
